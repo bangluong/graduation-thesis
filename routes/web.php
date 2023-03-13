968d96fc68afc9b28c2fbc8e446786e6f0e0518a
Nguyen Luong Bang <bangnl@bsscommerce.com>
2023-03-13 15:30:34 +0700

    create admin dashboard, login, admin user, install lang vi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\ChangePasswordController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InfoUserController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ResetController;
+use App\Http\Controllers\SessionsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +24,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/admin/', [HomeController::class, 'home']);
+    Route::get('admin/dashboard', function () {
+        return view('admin/dashboard');
+    })->name('dashboard');
+
+    Route::get('admin/user-management', function () {
+        return view('admin.user.user-management');
+    })->name('user-management');
+
+    Route::get('/admin/logout', [SessionsController::class, 'destroy']);
+    Route::get('/admin/user-profile', [InfoUserController::class, 'create']);
+    Route::post('/admin/user-profile', [InfoUserController::class, 'store']);
+    Route::get('/admin/login', function () {
+        return view('admin/dashboard');
+    })->name('sign-up');
+});
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/admin/register', [RegisterController::class, 'create']);
+    Route::post('/register', [RegisterController::class, 'store']);
+    Route::get('/admin/login', [SessionsController::class, 'create']);
+    Route::post('/session', [SessionsController::class, 'store']);
+    Route::get('/admin/login/forgot-password', [ResetController::class, 'create']);
+    Route::post('/forgot-password', [ResetController::class, 'sendEmail']);
+    Route::get('/admin/reset-password/{token}', [ResetController::class, 'resetPass'])->name('password.reset');
+    Route::post('/reset-password', [ChangePasswordController::class, 'changePassword'])->name('password.update');
+
+});
+
+Route::get('/admin/login', function () {
+    return view('session/login-session');
+})->name('login');
